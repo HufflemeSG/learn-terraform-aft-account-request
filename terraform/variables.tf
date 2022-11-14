51b10c61dad08ec4d31968ce58e13e751ccb2780
Pip <ppiper@sportinggroup.co.uk>
2022-11-14 22:49:29 +0000

    E-mail change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "Account_EMail" {
   description = "New e-mail address, not currently in AWS. root for sandbox account"
   type = string
-  default = "Gamorra@ymail.com"
+  default = "FingleFactor@yahoo.com"
 }
 
 variable "SSO_EMail" {
