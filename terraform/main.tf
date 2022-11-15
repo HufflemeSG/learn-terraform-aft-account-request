d3b8749fa1953884a12e9b1672e73a8da84d844a
Pip <ppiper@sportinggroup.co.uk>
2022-11-15 23:21:50 +0000

    E-mail change, add sandier box

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sanderbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "${var.Account_EMaili2}"
+    AccountName               = "sandierbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "${var.SSO_EMail}"
+    SSOUserFirstName          = "Sandierbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandierbox"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,12 @@ variable "Account_EMail" {
   default = "aws-test-1@sportinggroup.co.uk"
 }
 
+variable "Account_EMail2" {
+  description = "New e-mail address, not currently in AWS. root for sandbox account"
+  type = string
+  default = "aws-test-3@sportinggroup.co.uk"
+}
+
 variable "SSO_EMail" {
   description = "Same mail as AFT Management account"
   type = string
