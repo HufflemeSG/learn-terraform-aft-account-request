1b96dbcc16cd9214271be5a1f5e35498af1c5680
Pip <ppiper@sportinggroup.co.uk>
2023-02-24 10:17:59 +0000

    Commented out main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,55 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
+#module "sandbox" {
+#  source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "${var.Account_EMail}"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "${var.SSO_EMail}"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
+#  control_tower_parameters = {
+#    AccountEmail              = "${var.Account_EMail}"
+#    AccountName               = "sandbox-aft"
+#    ManagedOrganizationalUnit = "Learn AFT"
+#    SSOUserEmail              = "${var.SSO_EMail}"
+#    SSOUserFirstName          = "Sandbox"
+#    SSOUserLastName           = "AFT"
+#  }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#  account_tags = {
+#    "Learn Tutorial" = "AFT"
+#  }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#  change_management_parameters = {
+#    change_requested_by = "HashiCorp Learn"
+#    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#  }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#  custom_fields = {
+#    group = "non-prod"
+#  }
 
-  account_customizations_name = "sandbox"
-}
+#  account_customizations_name = "sandbox"
+#}
 
-module "sandierbox" {
-  source = "./modules/aft-account-request"
+#module "sandierbox" {
+#  source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "${var.Account_EMail2}"
-    AccountName               = "sandierbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "${var.SSO_EMail}"
-    SSOUserFirstName          = "Sandierbox"
-    SSOUserLastName           = "AFT"
-  }
+#  control_tower_parameters = {
+#    AccountEmail              = "${var.Account_EMail2}"
+#    AccountName               = "sandierbox-aft"
+#    ManagedOrganizationalUnit = "Learn AFT"
+#    SSOUserEmail              = "${var.SSO_EMail}"
+#    SSOUserFirstName          = "Sandierbox"
+#    SSOUserLastName           = "AFT"
+#  }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#  account_tags = {
+#    "Learn Tutorial" = "AFT"
+#  }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#  change_management_parameters = {
+#    change_requested_by = "HashiCorp Learn"
+#    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#  }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#  custom_fields = {
+#    group = "non-prod"
+#  }
 
-  account_customizations_name = "sandierbox"
-}
+#  account_customizations_name = "sandierbox"
+#}
