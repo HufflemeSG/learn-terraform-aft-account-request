dcf37d9a11e35b4a5194e7bfe58d00c31e469675
Pip <ppiper@sportinggroup.co.uk>
2023-02-24 10:13:11 +0000

    Commented out variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
-variable "Account_EMail" {
-  description = "New e-mail address, not currently in AWS. root for sandbox account"
-  type = string
-  default = "aws-test-1@sportinggroup.co.uk"
-}
+#variable "Account_EMail" {
+#  description = "New e-mail address, not currently in AWS. root for sandbox account"
+#  type = string
+#  default = "aws-test-1@sportinggroup.co.uk"
+#}
 
-variable "Account_EMail2" {
-  description = "New e-mail address, not currently in AWS. root for sandbox account"
-  type = string
-  default = "aws-test-3@sportinggroup.co.uk"
-}
+#variable "Account_EMail2" {
+#  description = "New e-mail address, not currently in AWS. root for sandbox account"
+#  type = string
+#  default = "aws-test-3@sportinggroup.co.uk"
+#}
 
-variable "SSO_EMail" {
-  description = "Same mail as AFT Management account"
-  type = string
-  default = "ppiper@sportinggroup.co.uk"
-}
+#variable "SSO_EMail" {
+#  description = "Same mail as AFT Management account"
+#  type = string
+#  default = "ppiper@sportinggroup.co.uk"
+#}
